80c2109c40cafc8a7896f7e386b8976552d0c668
Junseo <junseoadobe@gmail.com>
2025-06-09 22:03:50 +0900

    CloneManager

--- Snake/ConsoleApp5/CloneManager.cs
+++ Snake/ConsoleApp5/CloneManager.cs
@@ -1,62 +1,61 @@
-﻿class CloneManager
-{
-    public static CloneManager Instance = new CloneManager();
-    private List<(int x, int y)> clones = new List<(int x, int y)>();
-    public int Cooldown { get; set; } = 0;
+﻿using SnakeGameRefactored;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    private CloneManager() { }
-
-    public void Initialize()
+namespace SnakeGameRefactored
+{ // CloneManager: 복제체 관련 처리
+    public static class CloneManager
     {
-        clones.Clear();
-        Cooldown = 0;
-    }
+        public static List<(int x, int y)> Clones = new List<(int x, int y)>();
+        public static int CloneCooldown = 0;
 
-    public void SpawnClone(int x, int y)
-    {
-        clones.Add((x, y));
-    }
-
-    public void Update()
-    {
-        if (Cooldown > 0) Cooldown--;
-
-        for (int i = clones.Count - 1; i >= 0; i--)
+        public static void UpdateClones()
         {
-            var c = clones[i];
-            int[] directionWeights = { -1, -1, -1, -1, 0, 0, 1, -2, -2, +2, +2 };
-            int totalWeight = directionWeights.Length;
-            int choice = GameManager.Instance.Tick % totalWeight;
-            int dx = 0, dy = 0;
-
-            switch (directionWeights[choice])
+            for (int i = Clones.Count - 1; i >= 0; i--)
             {
-                case -2: dx = -1; break;
-                case -1: dy = -1; break;
-                case 0: break;
-                case +1: dy = +1; break;
-                case +2: dx = +1; break;
+                var c = Clones[i];
+                int[] directionWeights = { -1, -1, -1, -1, 0, 0, 1, -2, -2, +2, +2 };
+                int totalWeight = directionWeights.Length;
+                int choice = GameManager.Rand.Next(totalWeight);
+                int dx = 0, dy = 0;
+                switch (directionWeights[choice])
+                {
+                    case -2: dx = -1; break;
+                    case -1: dy = -1; break;
+                    case 0: break;
+                    case +1: dy = +1; break;
+                    case +2: dx = +1; break;
+                }
+                int nx = c.x + dx;
+                int ny = c.y + dy;
+                bool hitWall = false;
+                if (nx <= 0 || nx >= GameManager.Width - 1)
+                {
+                    dx = -dx;
+                    nx = c.x + dx;
+                    hitWall = true;
+                }
+                if (ny <= 0 || ny >= GameManager.Height - 1)
+                {
+                    dy = -dy;
+                    ny = c.y + dy;
+                    hitWall = true;
+                }
+                if (nx > 0 && nx < GameManager.Width - 1 && ny > 0 && ny < GameManager.Height - 1)
+                {
+                    if (MapManager.Map[ny, nx] == '#')
+                        MapManager.Map[ny, nx] = ' ';
+                    Clones[i] = (nx, ny);
+                }
+                else if (!hitWall)
+                {
+                    Clones.RemoveAt(i);
+                }
             }
-
-            int nx = c.x + dx;
-            int ny = c.y + dy;
-
-            if (nx <= 0 || nx >= GameManager.Width - 1)
-                dx = -dx;
-            if (ny <= 0 || ny >= GameManager.Height - 1)
-                dy = -dy;
-
-            nx = c.x + dx;
-            ny = c.y + dy;
-
-            if (MapManager.Instance.GetCharAt(ny, nx) == ' ')
-            {
-                clones[i] = (nx, ny);
-            }
-            else
-            {
-                clones.RemoveAt(i);
-            }
+            if (CloneCooldown > 0) CloneCooldown--;
         }
     }
 }
